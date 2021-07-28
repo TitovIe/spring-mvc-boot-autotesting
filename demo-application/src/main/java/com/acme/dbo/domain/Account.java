c04f5bdce5b824abad92e4eb63959a8b4820fdfd
Ilya <titov.ie@phystech.edu>
2021-07-29 00:32:36 +0300

    Add some tests and constructor, equal method in Account.java

--- demo-application/src/main/java/com/acme/dbo/domain/Account.java
+++ demo-application/src/main/java/com/acme/dbo/domain/Account.java
@@ -1,6 +1,7 @@
 package com.acme.dbo.domain;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Account {
     private int id;
@@ -9,6 +10,10 @@ public class Account {
     public Account(int id, BigDecimal amount) {
         this.id = id;
         this.amount = amount;
+    }
+
+    public Account(BigDecimal bigDecimal) {
+        this.amount = bigDecimal;
     }
 
     public int getId() {
@@ -22,5 +27,18 @@ public class Account {
     @Override
     public String toString() {
         return "Account " + getId() + " : " + getAmount();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return id == account.id && Objects.equals(amount, account.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, amount);
     }
 }
--- demo-application/src/test/java/com/acme/dbo/AccountCrudSystemIT.java
+++ demo-application/src/test/java/com/acme/dbo/AccountCrudSystemIT.java
@@ -2,12 +2,19 @@ package com.acme.dbo;
 
 import com.acme.dbo.config.Config;
 import com.acme.dbo.controller.AccountController;
+import com.acme.dbo.controller.AccountNotFoundException;
+import com.acme.dbo.dao.AccountRepository;
+import com.acme.dbo.domain.Account;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
@@ -17,9 +24,26 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @ContextConfiguration(classes = Config.class)
 public class AccountCrudSystemIT {
     @Autowired private AccountController accountController;
+    @Autowired private AccountRepository accountRepository;
 
     @Test
     public void shouldGetNoAccountsWhenNoCreated() {
         assertTrue(accountController.findAll().isEmpty());
+    }
+
+    @Test
+    @DirtiesContext
+    public void shouldGetAccountWhenFindByIdAndExist() throws AccountNotFoundException {
+        Account account = new Account(new BigDecimal("0.15"));
+        accountRepository.create(account);
+        assertEquals(accountController.findById(account.getId()), new Account(account.getId(), new BigDecimal("0.15")));
+    }
+
+    @Test
+    @DirtiesContext
+    public void shouldGetAccountWhenFindAllAndExists() {
+        Account account = new Account(new BigDecimal("1.15"));
+        accountRepository.create(account);
+        assertTrue(accountController.findAll().contains(new Account(account.getId(), new BigDecimal("1.15"))));
     }
 }
