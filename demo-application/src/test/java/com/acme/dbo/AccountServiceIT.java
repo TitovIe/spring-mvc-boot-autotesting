c48351b00ddbc8ed807a3ca721933f9a1c06b0f5
Ilya <titov.ie@phystech.edu>
2021-08-08 16:04:21 +0300

    Add several tests in spring boot configuration

--- demo-application/src/test/java/com/acme/dbo/AccountServiceIT.java
+++ demo-application/src/test/java/com/acme/dbo/AccountServiceIT.java
@@ -8,8 +8,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
+import org.springframework.test.annotation.DirtiesContext;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -20,11 +23,19 @@ public class AccountServiceIT {
     @Autowired private AccountService accountService;
 
     @Test
+    @DirtiesContext
     public void shouldFindByIdWhenExists() throws AccountNotFoundException {
         accountRepository.save(new Account(new BigDecimal("1.11")));
 
         assertEquals(
                 new Account(1, new BigDecimal("1.11")),
                 accountService.findById(1));
+    }
+
+    @Test
+    @DirtiesContext
+    public void shouldFindAllWhenExists() {
+        accountRepository.saveAll(Arrays.asList(new Account(new BigDecimal("1.11")), new Account(new BigDecimal("2.22"))));
+        assertEquals(((ArrayList<Account>) accountService.findAll()).size(), 2);
     }
 }
--- demo-application/src/test/java/com/acme/dbo/AccountServiceTest.java
+++ demo-application/src/test/java/com/acme/dbo/AccountServiceTest.java
@@ -1,5 +1,6 @@
 package com.acme.dbo;
 
+import com.acme.dbo.controller.AccountNotFoundException;
 import com.acme.dbo.dao.AccountRepository;
 import com.acme.dbo.domain.Account;
 import com.acme.dbo.service.AccountService;
@@ -13,11 +14,14 @@ import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 /**
  * Look ma, no Spring at all!
@@ -29,19 +33,51 @@ public class AccountServiceTest {
      */
     @Mock private AccountRepository accountRepositoryStub;
     @Mock private RestTemplateBuilder restTemplateBuilder;
-    @Mock RestTemplate restTemplateStub;
+    @Mock private RestTemplate restTemplateStub;
     @InjectMocks private AccountService sut;
 
     @Test
     public void shouldGetNoAccountsWhenEmptyRepository() {
+        when(accountRepositoryStub.findAll()).thenReturn(Collections.emptyList());
+
+        List<Account> accounts = new ArrayList<>();
+        Iterable<Account> accountIterable = sut.findAll();
+        accountIterable.forEach(accounts::add);
+
+        verify(accountRepositoryStub).findAll();
+
+        assertTrue(accounts.isEmpty());
+    }
+
+    @Test
+    public void shouldGetAccountsWhenNoEmptyRepository() {
         when(accountRepositoryStub.findAll()).thenReturn(
-                asList(new Account(2, new BigDecimal("2.22"))));
+                Collections.singletonList(new Account(2, new BigDecimal("2.22"))));
 
         List<Account> accounts = new ArrayList<>();
         Iterable<Account> accountsIterator = sut.findAll();
         accountsIterator.forEach(accounts::add);
 
+        verify(accountRepositoryStub).findAll();
+
         assertTrue(accounts.contains(
                 new Account(2, new BigDecimal("2.22"))));
+    }
+
+    @Test
+    public void shouldGetAccountWhenFindById() throws AccountNotFoundException {
+        when(accountRepositoryStub.findById(1)).thenReturn(java.util.Optional.of(new Account(1, new BigDecimal("1.1"))));
+
+        assertEquals(sut.findById(1), new Account(1, new BigDecimal("1.1")));
+        verify(accountRepositoryStub).findById(1);
+    }
+
+    @Test
+    public void shouldGetAccountWhenSave() {
+        Account account = new Account(2, new BigDecimal("2.22"));
+        when(accountRepositoryStub.save(account)).thenReturn(account);
+
+        assertEquals(sut.create(account), account);
+        verify(accountRepositoryStub).save(account);
     }
 }
